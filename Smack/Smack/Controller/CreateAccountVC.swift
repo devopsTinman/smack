e13666394229f5cac30b0cd5a76b30c0de83e4a0
DevOps Tinman <devops.tinman@gmail.com>
2019-01-18 17:30:43 -0600

    added create account

---
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,21 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by Allbee, Eamon on 1/18/19.
+//  Copyright © 2019 Allbee, Eamon. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+ 
+
+}
--- Smack/Smack/Controller/LoginVC.swift
+++ Smack/Smack/Controller/LoginVC.swift
@@ -22,6 +22,10 @@ class LoginVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func createAccountBtnPressed(_ sender: Any) {
+       performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+        
+    }
+    
 
 }
