38767180c6e31f4bed73018c7d5241d4de33c747
DevOps Tinman <devops.tinman@gmail.com>
2019-01-23 11:18:55 -0600

    Added Avatar Picker VC

---
+++ Smack/Smack/View/AvatarCell.swift
@@ -0,0 +1,44 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Allbee, Eamon on 1/23/19.
+//  Copyright © 2019 Allbee, Eamon. All rights reserved.
+//
+
+import UIKit
+
+enum AvatarType {
+    case dark
+    case light
+}
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+        
+    }
+    
+    func configureCell(index: Int, type: AvatarType) {
+        if type == AvatarType.dark {
+            avatarImg.image = UIImage(named: "dark\(index)")
+            self.layer.backgroundColor = UIColor.lightGray.cgColor
+        }
+        else {
+            avatarImg.image = UIImage(named: "light\(index)")
+            self.layer.backgroundColor = UIColor.gray.cgColor
+        }
+    }
+    
+    func setupView() {
+        
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+        
+    }
+}
